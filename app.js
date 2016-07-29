2c6ff5474e3a475eddf3e0f3fcc5f4d7dbdd7067
georgecr0ss <george.krystew@gmail.com>
2016-07-30 01:04:27 +0300

    adding bootstrapzero theme

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var express = require('express'),
 	app = express();
 
+
 const PORT = 5000;
+
+//app.use allows us to set middleware, 
+app.use(express.static('public'));
+app.use(express.static('src/views'));
 
 app.get('/', function(req, res){
 	res.send('Hello World')
