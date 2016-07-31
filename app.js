d682551420dbcfa296c3d03c4a50996c8cc7101d
georgecr0ss <george.krystew@gmail.com>
2016-07-31 23:04:41 +0300

    c

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 var express = require('express');
 
 var app = express();
-var booksRouter = require('./src/routes/bookRoutes');
 
 var PORT = process.env.PORT || 5000;
-// app.use(app.router);
-//app.use allows us to set middleware,
+var nav =  [{
+    Link: '/Books',
+    Text: 'Book'
+}, {
+    Link: '/Authors',
+    Text: 'Author'
+}];
+var booksRouter = require('./src/routes/bookRoutes')(nav); 
 
 app.use(express.static('public'));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
+
 
 app.use('/Books', booksRouter);
 app.get('/books', function(req, res) {
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,75 +1,61 @@
 var express = require('express');
 var booksRouter = express.Router();
+var router = function(nav) {
 
-var books = [{
-    title: 'Tom Sawyer',
-    author: 'Mark Twain',
-    read: false
-},{
-    title: 'The Prince and the Pauper',
-    author: 'Mark Twain',
-    read: false
-},{
-    title: 'До Чикаго и назад',
-    author: 'Алеко Константинов',
-    read: false
-},{
-    title: 'Бай Ганьо',
-    author: 'Алеко Константинов',
-    read: false
-},{
-    title: 'Под игото',
-    author: 'Иван Вазов',
-    read: false
-},{
-    title: 'Мамино детенце',
-    author: 'Любен Каравелов',
-    read: false
-}];
+    var books = [{
+        title: 'Tom Sawyer',
+        author: 'Mark Twain',
+        read: false
+        },{
+        title: 'The Prince and the Pauper',
+        author: 'Mark Twain',
+        read: false
+        },{
+        title: 'До Чикаго и назад',
+        author: 'Алеко Константинов',
+        read: false
+        },{
+        title: 'Бай Ганьо',
+        author: 'Алеко Константинов',
+        read: false
+        },{
+        title: 'Под игото',
+        author: 'Иван Вазов',
+        read: false
+        },{
+        title: 'Мамино детенце',
+        author: 'Любен Каравелов',
+        read: false
+    }];
 
-booksRouter.route('/')
-	.get(function (req, res) {
-    res.render('bookListView', {
-        title: 'Portfolio',
-        nav: [{
-            Link: '/Books',
-            Text: 'Authors'
-        }, {
-            Link: '/Mark Twain',
-            Text: 'Mark Twain'
-        }],
-        books: books
+    booksRouter.route('/')
+        .get(function (req, res) {
+        res.render('bookListView', {
+            title: 'Portfolio',
+            nav: nav,
+            books: books
+        });
     });
-});
-booksRouter.route('/:id')
-	.get(function(req, res) {
-    var id =  req.params.id;
-    res.render('bookView', {
-        title: 'Portfolio',
-        nav: [{
-            Link: '/Books',
-            Text: 'Authors'
-        }, {
-            Link: '/Mark Twain',
-            Text: 'Mark Twain'
-        }],
-        books: books[id]
+    booksRouter.route('/:id')
+        .get(function(req, res) {
+        var id =  req.params.id;
+        res.render('bookView', {
+            title: 'Portfolio',
+            nav: nav,
+            books: books[id]
+        });
     });
-});
 
-// app.use(express.static('src/views'));
+    // app.use(express.static('src/views'));
 
-booksRouter.get('/', function(req, res) {
-    res.render('index', {
-        title: 'Portfolio',
-        nav: [{
-            Link: '/Books',
-            Text: 'Books'
-        }, {
-            Link: '/Authors',
-            Text: 'Authors'
-        }]
+    booksRouter.get('/', function(req, res) {
+        res.render('index', {
+            title: 'Portfolio',
+            nav: nav
+        });
     });
-});
 
-module.exports = booksRouter;
+    return booksRouter;
+}
+
+module.exports = router;
